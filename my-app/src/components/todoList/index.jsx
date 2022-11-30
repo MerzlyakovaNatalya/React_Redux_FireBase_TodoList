9c4ee21d830904398225c6922a9f333140445771
MerzlyakovaNatalya <94801202+MerzlyakovaNatalya@users.noreply.github.com>
2022-11-30 14:30:38 +0300

    add_file

--- my-app/src/components/todoList/index.jsx
+++ my-app/src/components/todoList/index.jsx
@@ -7,8 +7,11 @@ let localizedFormat = require("dayjs/plugin/localizedFormat");
 
 export const TodoList = () => {
   const [data, setData] = useState("");
+  const [valueTitle, setValueTitle] = useState("");
+  const [valueContent, setValueContent] = useState("");
+  const [valueFile, setValueFile] = useState();
 
-  const handleOnChange = (event) => {
+  const handleOnChangeData = (event) => {
     event.preventDefault();
     setData(event.target.value);
   };
@@ -23,20 +26,54 @@ export const TodoList = () => {
     }
   };
 
+  const onChangeTitle = (event) => {
+    event.preventDefault();
+    setValueTitle(event.target.value);
+  }
+
+  const onChangeContent = (event) => {
+    event.preventDefault();
+    setValueContent(event.target.value);
+  }
+
+  const onChangeFile = (event) => {
+    event.preventDefault();
+    const file = event.target.files[0]
+    setValueFile(file);
+  }
+  const onSend = () => {
+
+  }
+
+  const resetForm = () => {
+
+  }
+
   return (
     <div className={style.wrap}>
-      <form className={style.wrap_form}>
-        <input type="text" placeholder="Задача" />
+      <form className={style.wrap_form} onSubmit={() => {
+        onSend()
+        resetForm()}}>
+        <input 
+          type="text" 
+          placeholder="Задача" 
+          onChange={onChangeTitle}/>
         <textarea
           name="text"
           placeholder="Описание задачи"
+          onChange={onChangeContent}
         ></textarea>
-        <label htmlFor="file-loader-button" className={style.label_file}>Выбрать файл</label>
-        <input type="file" id="file-loader-button" className={style.label_button}/>
-        <button className={style.buttons} >Отправить</button>
+        <label htmlFor="file-loader-button" className={style.label_file}>{valueFile?valueFile.name:"Выбрать файл"}</label>
+        <input 
+        type="file" 
+        id="file-loader-button" 
+        className={style.label_button}
+        onChange={onChangeFile}
+        />
+        <button className={style.buttons} type="submit">Отправить</button>
       </form>
       <TodoItem
-      handleOnChange={handleOnChange}
+      handleOnChangeData={handleOnChangeData}
       getDateFormat={getDateFormat}
       />
     </div>
